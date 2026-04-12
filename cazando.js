1ad22fdda288fcc534692dfbbf73705ffa4e5e39
CesarGualpa <cesar_amagu@hotmail.com>
2026-04-11 19:26:49 -0500

    Parte 2.1

--- cazando.js
+++ cazando.js
@@ -2,7 +2,35 @@ let canvas;
 let ctx;
 
 function graficarGato() {
+    ctx.fillRect(gatoX, gatoY, ANCHO_GATO, ALTO_GATO);
+}
+
+function graficarComida() {
+    ctx.fillRect(comidaX, comidaY, ANCHO_COMIDA, ALTO_COMIDA);
+}
+
+// posiciones
+let gatoX = 0;
+let gatoY = 0;
+let comidaX = 0;
+let comidaY = 0;
+
+// tamaños
+const ANCHO_GATO = 50;
+const ALTO_GATO = 50;
+const ANCHO_COMIDA = 20;
+const ALTO_COMIDA = 20;
+
+function iniciarJuego() {
     canvas = document.getElementById("areaJuego");
     ctx = canvas.getContext("2d");
-    ctx.fillRect(225, 225, 50, 50);
+
+    gatoX = (canvas.width / 2) - (ANCHO_GATO / 2);
+    gatoY = (canvas.height / 2) - (ALTO_GATO / 2);
+
+    comidaX = canvas.width - ANCHO_COMIDA;
+    comidaY = canvas.height - ALTO_COMIDA;
+
+    graficarGato();
+    graficarComida();
 }
